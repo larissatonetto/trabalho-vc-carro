6bb66e54ac1bc44c2f4cf5f0c424d65c5bbb5a4b
Larissa Tonetto <larissatonetto0@gmail.com>
2023-09-29 03:30:04 -0300

    Funcionando +/-

--- video-capture.py
+++ video-capture.py
@@ -12,53 +12,36 @@ x_end = int(width / 2 + width / 4)
 y_start = int(height / 2 - height / 4)
 y_end = int(height / 2 + height / 4)
 
-def get_dominant_color(img):
-    img_hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
-    arr = np.asarray(img_hsv).astype(np.uint8)
-
-    # Cria um box de tamanho largura/2 * altura/2 no meio da tela
-    x_start = int(width / 2 - width / 4)
-    x_end = int(width / 2 + width / 4)
-    y_start = int(height / 2 - height / 4)
-    y_end = int(height / 2 + height / 4)
+def get_dominant_color(arr):
+    img_hsv = cv.cvtColor(arr, cv.COLOR_BGR2HSV)
 
     arr[y_start, x_start:x_end] = (0, 0, 0)
     arr[y_end, x_start:x_end] = (0, 0, 0)
     arr[y_start:y_end, x_start] = (0, 0, 0)
     arr[y_start:y_end, x_end] = (0, 0, 0)
 
-    hue = arr[y_start:y_end, x_start:x_end, 0]
-    sat = arr[y_start:y_end, x_start:x_end, 1]
-    val = arr[y_start:y_end, x_start:x_end, 2]
-
-    sum_black = np.sum(val < 30)
-    sum_red = np.sum((hue >= 0) & (hue < 60))
-    sum_green = np.sum((hue >= 60) & (hue < 120))
-    sum_blue = np.sum((hue >= 120) & (hue < 180))
+    b = arr[y_start:y_end, x_start:x_end, 0]
+    g = arr[y_start:y_end, x_start:x_end, 1]
+    r = arr[y_start:y_end, x_start:x_end, 2]
+    val = img_hsv[y_start:y_end, x_start:x_end, 2]
 
     colors = {0: "black", 1: "red", 2: "green", 3: "blue"}
     color_sums = [
         np.sum(val < 60),  # Preto
-        np.sum((hue >= 0) & (hue < 60)),  # Vermelho
-        np.sum((hue >= 60) & (hue < 120)),  # Verde
-        np.sum((hue >= 120) & (hue <= 180)),  # Azul
+        np.sum(np.sum((r > b) & (r > g) & (val > 50))),  # Vermelho
+        np.sum(np.sum((g > r) & (g > b) & (val > 50))),  # Verde
+        np.sum(np.sum((b > r) & (b > g) & (val > 50))),  # Azul
     ]
 
-    return (cv.cvtColor(arr, cv.COLOR_HSV2BGR), colors[np.argmax(color_sums)])
+    return (arr, colors[np.argmax(color_sums)])
 
 
 def show_color(arr, color):
-    # bgr_arr = np.asarray(img).astype(np.uint8)
     img_hsv = cv.cvtColor(arr, cv.COLOR_BGR2HSV)
 
-    # bgr_arr[y_start:y_end, x_start:x_end] = (255, 255, 255)
-    # bgr_arr[y_start, x_start:x_end] = (0, 0, 0)
-    # bgr_arr[y_end, x_start:x_end] = (0, 0, 0)
-    # bgr_arr[y_start:y_end, x_start] = (0, 0, 0)
-    # bgr_arr[y_start:y_end, x_end] = (0, 0, 0)
-
-    hue = img_hsv[:,:,0]
-    sat = img_hsv[:,:,1]
+    b = arr[:,:,0]
+    g = arr[:,:,1]
+    r = arr[:,:,2]
     val = img_hsv[:,:,2]
 
     new_arr = arr.copy()
@@ -66,27 +49,27 @@ def show_color(arr, color):
     if color == "black":
         new_arr[val > 50] = (255, 255, 255)
     elif color == "red":
-        new_arr[(hue >= 60) | (val <= 60)] = (255, 255, 255)
-    elif color == "blue":
-        new_arr[(hue < 60) | (hue >= 120) | (val <= 60)] = (255, 255, 255)
+        new_arr[(r < b) | (r < g) | (val < 50)] = (255, 255, 255)
+    elif color == "green":
+        new_arr[(g < r) | (g < b) | (val < 50)] = (255, 255, 255)
     else:
-        new_arr[(hue < 120) | (val <= 60)] = (255, 255, 255)
+        new_arr[(b < r) | (b < g) | (val < 50)] = (255, 255, 255)
 
     return new_arr
 
 
 def process_image(image):
     arr = np.asarray(image).astype(np.uint8)
-    new_img = show_color(arr[y_start:y_end, x_start:x_end], "green")
-    # print(color)
+    new_img, color = get_dominant_color(arr)
+    print("color = ", color)
     cv.imshow("frame", new_img)
 
 def process_image_debug(image):
     arr = np.asarray(image).astype(np.uint8)
-    img_black = show_color(arr[y_start:y_end, x_start:x_end], "black")
-    img_red = show_color(arr[y_start:y_end, x_start:x_end], "red")
-    img_green = show_color(arr[y_start:y_end, x_start:x_end], "green")
-    img_blue = show_color(arr[y_start:y_end, x_start:x_end], "blue")
+    img_black = show_color(arr, "black")
+    img_red = show_color(arr, "red")
+    img_green = show_color(arr, "green")
+    img_blue = show_color(arr, "blue")
 
     cv.imshow("black", img_black)
     cv.imshow("red", img_red)
@@ -127,8 +110,8 @@ while True:
         print("Can't receive frame (stream end?). Exiting ...")
         break
 
-    # process_image(frame)
-    process_image_debug(frame)
+    process_image(frame)
+    # process_image_debug(frame)
 
     print(datetime.utcnow().strftime("%F %T.%f")[:-3])
 
