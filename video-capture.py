70e171e4e7afbfe8d5d1df090121a7f5a027895c
Larissa Tonetto <larissatonetto0@gmail.com>
2023-09-29 02:59:15 -0300

    Funcionando para as cores preto e azul

--- video-capture.py
+++ video-capture.py
@@ -7,6 +7,10 @@ fps = 30.0
 height = 240.0
 width = 320.0
 
+x_start = int(width / 2 - width / 4)
+x_end = int(width / 2 + width / 4)
+y_start = int(height / 2 - height / 4)
+y_end = int(height / 2 + height / 4)
 
 def get_dominant_color(img):
     img_hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
@@ -34,7 +38,7 @@ def get_dominant_color(img):
 
     colors = {0: "black", 1: "red", 2: "green", 3: "blue"}
     color_sums = [
-        np.sum(val > 200),  # Preto
+        np.sum(val < 60),  # Preto
         np.sum((hue >= 0) & (hue < 60)),  # Vermelho
         np.sum((hue >= 60) & (hue < 120)),  # Verde
         np.sum((hue >= 120) & (hue <= 180)),  # Azul
@@ -43,45 +47,52 @@ def get_dominant_color(img):
     return (cv.cvtColor(arr, cv.COLOR_HSV2BGR), colors[np.argmax(color_sums)])
 
 
-def get_color(img, color):
-    bgr_arr = np.asarray(img).astype(np.uint8)
-    img_hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
-    arr = np.asarray(img_hsv).astype(np.uint8)
+def show_color(arr, color):
+    # bgr_arr = np.asarray(img).astype(np.uint8)
+    img_hsv = cv.cvtColor(arr, cv.COLOR_BGR2HSV)
 
-    # Cria um box de tamanho largura/2 * altura/2 no meio da tela
-    x_start = int(width / 2 - width / 4)
-    x_end = int(width / 2 + width / 4)
-    y_start = int(height / 2 - height / 4)
-    y_end = int(height / 2 + height / 4)
+    # bgr_arr[y_start:y_end, x_start:x_end] = (255, 255, 255)
+    # bgr_arr[y_start, x_start:x_end] = (0, 0, 0)
+    # bgr_arr[y_end, x_start:x_end] = (0, 0, 0)
+    # bgr_arr[y_start:y_end, x_start] = (0, 0, 0)
+    # bgr_arr[y_start:y_end, x_end] = (0, 0, 0)
 
-    bgr_arr[y_start:y_end, x_start:x_end] = (255, 255, 255)
-    bgr_arr[y_start, x_start:x_end] = (0, 0, 0)
-    bgr_arr[y_end, x_start:x_end] = (0, 0, 0)
-    bgr_arr[y_start:y_end, x_start] = (0, 0, 0)
-    bgr_arr[y_start:y_end, x_end] = (0, 0, 0)
+    hue = img_hsv[:,:,0]
+    sat = img_hsv[:,:,1]
+    val = img_hsv[:,:,2]
 
-    hue = arr[y_start:y_end, x_start:x_end, 0]
-    sat = arr[y_start:y_end, x_start:x_end, 1]
-    val = arr[y_start:y_end, x_start:x_end, 2]
+    new_arr = arr.copy()
 
     if color == "black":
-        print(1)
+        new_arr[val > 50] = (255, 255, 255)
     elif color == "red":
-        print(2)
-    elif color == "green":
-        print(3)
+        new_arr[(hue >= 60) | (val <= 60)] = (255, 255, 255)
+    elif color == "blue":
+        new_arr[(hue < 60) | (hue >= 120) | (val <= 60)] = (255, 255, 255)
     else:
-        print(4)
+        new_arr[(hue < 120) | (val <= 60)] = (255, 255, 255)
 
-    return bgr_arr
+    return new_arr
 
 
 def process_image(image):
-    new_img, color = get_dominant_color(image)
-    # new_img = get_color(image, "black")
-    print(color)
+    arr = np.asarray(image).astype(np.uint8)
+    new_img = show_color(arr[y_start:y_end, x_start:x_end], "green")
+    # print(color)
     cv.imshow("frame", new_img)
 
+def process_image_debug(image):
+    arr = np.asarray(image).astype(np.uint8)
+    img_black = show_color(arr[y_start:y_end, x_start:x_end], "black")
+    img_red = show_color(arr[y_start:y_end, x_start:x_end], "red")
+    img_green = show_color(arr[y_start:y_end, x_start:x_end], "green")
+    img_blue = show_color(arr[y_start:y_end, x_start:x_end], "blue")
+
+    cv.imshow("black", img_black)
+    cv.imshow("red", img_red)
+    cv.imshow("green", img_green)
+    cv.imshow("blue", img_blue)
+    cv.imshow("original", image)
 
 # Change the camera id
 cap = cv.VideoCapture(0)
@@ -116,7 +127,8 @@ while True:
         print("Can't receive frame (stream end?). Exiting ...")
         break
 
-    process_image(frame)
+    # process_image(frame)
+    process_image_debug(frame)
 
     print(datetime.utcnow().strftime("%F %T.%f")[:-3])
 
